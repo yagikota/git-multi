dc85ef4f268c6fb875a887a02980b3099ce389d1
Kota <mdryzk64smsh@gmail.com>
2023-03-20 23:46:56 +0900

    add: ✅ test (#1)
    * add: ✅ test
    
    * fix: 💚 CI
    
    * fix: 💚 CI
    
    * fix: 💚 CI
    
    * fix: 💚 CI
    
    * fix: 💚 CI
    
    * fix: 💚 CI
    
    * fix: 💚 CI
    
    * fix: 💚 CI

--- pkg/multiclone/multiclone_test.go
+++ pkg/multiclone/multiclone_test.go
@@ -7,6 +7,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestIsSSHURL(t *testing.T) {
+	tests := []struct {
+		name string
+		url  string
+		want bool
+	}{
+		{
+			name: "valid ssh url",
+			url:  "git@github.com:username/repo.git",
+			want: true,
+		},
+		{
+			name: "invalid ssh url(prefix miss match)",
+			url:  "https://github.com/username/repo.git",
+			want: false,
+		},
+		{
+			name: "invalid ssh url(suffix miss match)",
+			url:  "git@github.com:username/repo",
+			want: false,
+		},
+		{
+			name: "invalid url(prefix and suffix miss match)",
+			url:  "invalid-url",
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := isSSHURL(tt.url)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
 func TestRepoHTTPSURLFromSSHURL(t *testing.T) {
 	tests := []struct {
 		name    string
